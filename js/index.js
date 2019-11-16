c31c6b2f9cdb45481d8e530e154360103006c17b
eanchan0310 <56179170+eanchan0310@users.noreply.github.com>
2019-11-16 12:54:11 +0900

    .
    .

--- js/index.js
+++ js/index.js
@@ -8,10 +8,19 @@ var config = {
     },
     parent: 'phaser-example',
     scene: {
+        preload: preload,
         create: create,
         update: update
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
     }
 };
+document.body.style.backgroundColor = "green";        
+
 
  // // 시간 지연 실행
     // var _tmpDelay = 3000;
@@ -20,9 +29,10 @@ var config = {
     // }, _tmpDelay)
 
 // 전역 변수 선언
-var delay = 10000;
+var delay = 3000;
 var game = new Phaser.Game(config);
 var player;
+var start;
 var fire;
 var random = 0;
 var conTU = 0;
@@ -32,6 +42,12 @@ var i=0;
 var FireList = [10];
 // 전역 변수 선언 끝
 
+function preload(){
+    this.load.image('Fire', './assets/image/Fire_1.jpg');
+    this.load.image('chicken', './assets/image/chicken_1.jpg');
+
+
+}
 // create
 function create() 
 {    
@@ -44,11 +60,61 @@ function create()
     var pointer1 = game.input.activePointer;
     // 변수 선언 끝
 
-    // 플레이어(닭) 설정
-    player = this.add.text(100, 200, '  ===\n+  !   =\n  ===\n =     ==\n ======', { fontFamily: 'Arial', fontSize: 20, color: '#00ff00' }).setInteractive();
+    player = this.physics.add.image(100, 200, 'chicken').setInteractive();
+    // player = this.physics.add.text(100, 200, '  ===\n+  !   =\n  ===\n =     ==\n ======', { fontFamily: 'Arial', fontSize: 20, color: '#00ff00' }).setInteractive();
+    player.setDisplaySize(30);
     player.setOrigin(0.5);
     this.input.setDraggable(player);
+    init_player(false);   
 
+    GameOver = this.add.text(400, 300, 'GameOver', { fontFamily: 'Arial', fontSize: 350, color: 'gray' });
+    GameOver.setVisible(false);
+
+    for(i=0; i<5; i++){
+        FireList[i] = this.physics.add.image(0, Random_Int(500, 10), 'Fire');
+        FireList[i].setOrigin(0.5);
+        FireList[i].setDisplaySize(50);
+    }
+    for(i=0; i<5; i++){
+        FireList[i+5] = this.physics.add.image(Random_Int(700, 10), 0, 'Fire');
+        // FireList[i+5] = this.physics.add.text(Random_Int(700, 10), 0, 'O', { fontFamily: 'Arial', fontSize: Random_Int(80, 30), color: 'red' });
+        FireList[i+5].setOrigin(0.5);
+        FireList[i+5].setDisplaySize(50);
+    }
+    init_Fire(false);
+
+    start = this.add.text(400, 480, 'start', { fontFamily: 'Arial', fontsize: 200, color: 'blue' });
+    start.setInteractive();
+
+    start.on('pointerdown', (pointer) => {
+        start.setVisible(false);
+        setTimeout( () => {
+            console.log(player.y, FireList[3].y);
+            init_player(true);
+            init_Fire(true);
+            for(i=0; i<5; i++){
+                tween_LR.call(this, FireList[i]);
+            }
+            for(i=5; i<10; i++){
+                tween_UD.call(this, FireList[i]);
+            }
+        }, delay)
+    });
+
+    
+ // 까지 생성
+    
+    for(var k=0; k<10; k++){
+        var emp = FireList[k];
+        this.physics.add.collider(emp, player, () => {
+            init_Fire(false);
+            // console.log(emp);
+
+        })
+    }
+
+
+    
     this.input.on('pointerdown', function (pointer, gameObject) {
         xdf = player.x - pointer.x;
         ydf = player.y - pointer.y;
@@ -76,76 +142,71 @@ function create()
 
         return _pointerY + ydf;
     }
-    // 플레이어(닭) 설정 끝
     
-    //불꽃 설정
-    for(i=0; i<5; i++){
-        FireList[i] = this.add.text(0, Random_Int(500, 10), 'O', { fontFamily: 'Arial', fontSize: Random_Int(80, 30), color: 'red' });
-        FireList[i].setOrigin(0.5);
-    }
-    for(i=0; i<5; i++){
-        FireList[i+5] = this.add.text(Random_Int(700, 10), 0, 'O', { fontFamily: 'Arial', fontSize: Random_Int(80, 30), color: 'red' });
-        FireList[i+5].setOrigin(0.5);
-    }
-    setTimeout( () => {
-        for(i=0; i<5; i++){
-            tween_LR.call(this, FireList[i]);
-        }
-        for(i=5; i<10; i++){
-            tween_UD.call(this, FireList[i]);
-        }
-    }, delay)
-
+        
+    
 
     
     
 
-    function tween_LR(target) {
-        var tween1 = this.tweens.add({
-            targets: target,
-            x: 800, 
-            duration: Random_Int(5,1)*1000, 
-            ease: 'Linear',
-            yoyo: true,
-            onYoyo: function () { 
-                target.y = Random_Int(600, 0)
-                console.log('yoyo');
-            },
-            onComplete: () => { 
-                target.y = Random_Int(600, 0)
-                console.log('com')
-                tween_LR.call(this, target);
-            }
-        });
-    }
-    function tween_UD(target) {
-        var tween1 = this.tweens.add({
-            targets: target,
-            y: 600, 
-            duration: Random_Int(5,1)*1000, 
-            ease: 'Linear',
-            yoyo: true,
-            onYoyo: function () { 
-                target.x = Random_Int(800, 0)
-                console.log('yoyo');
-            },
-            onComplete: () => { 
-                target.x = Random_Int(800, 0)
-                console.log('com')
-                tween_UD.call(this, target);
-            }
-        });
-    }
+    
     //불꽃 설정 끝
 }
 // create 끝
+
+function init_player(bool) {
+    player.setVisible(bool);
+    player.setActive(bool);  
+}
+function init_Fire(bool) {
+    for(var j=0; j<10; j++){
+        FireList[j].setVisible(bool);
+        FireList[j].setActive(bool);        
+    }
+}
+function tween_LR(target) {
+    var tween1 = this.tweens.add({
+        targets: target,
+        x: 800, 
+        duration: Random_Int(5,1)*1000, 
+        ease: 'Linear',
+        yoyo: true,
+        onYoyo: function () { 
+            target.y = Random_Int(600, 0)
+            // console.log('yoyo');
+        },
+        onComplete: () => { 
+            target.y = Random_Int(600, 0)
+            // console.log('com')
+            tween_LR.call(this, target);
+        }
+    });
+}
+function tween_UD(target) {
+    var tween1 = this.tweens.add({
+        targets: target,
+        y: 600, 
+        duration: Random_Int(5,1)*1000, 
+        ease: 'Linear',
+        yoyo: true,
+        onYoyo: function () { 
+            target.x = Random_Int(800, 0)
+            // console.log('yoyo');
+        },
+        onComplete: () => { 
+            target.x = Random_Int(800, 0)
+            // console.log('com')
+            tween_UD.call(this, target);
+        }
+    });
+}
 
 //랜덤 함수 선언
 function Random_Int(max, min) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
 
 function Random_Double (max, min){
     return Math.random() * (max - min) + min;
@@ -230,7 +291,7 @@ function change() {
         default:
             break;
     }
-    console.log("random은 ",random);
+    // console.log("random은 ",random);
 }
 
 function Right_to_Left(){
